ca89b1ffaa4faebf9842fdb772510122c72aeee2
tsszh <songzhpku@gmail.com>
2015-10-31 14:25:10 -0500

    Refactoring To Obey OOP

--- src/main/java/com/songzh/jenkins_slack/BotDemo.java
+++ src/main/java/com/songzh/jenkins_slack/BotDemo.java
@@ -13,45 +13,66 @@ import com.ullink.slack.simpleslackapi.listeners.SlackMessagePostedListener;
 
 
 public class BotDemo {
-	String url = "https://fa15-cs427-082.cs.illinois.edu:8083/";
-	static JenkinsServer jenkins;
-	static JenkinsHttpClient conn;
-	public static void main(String[] args) {
-		
-		try {
-			conn = new JenkinsHttpClient(new URI("https://fa15-cs427-032.cs.illinois.edu:8083/"), "zsong12", "w2jkscs427");
-			jenkins = new JenkinsServer(conn);
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		SlackSession session = SlackSessionFactory.createWebSocketSlackSession("xoxb-13203396052-6U3vsnm89DThgtCw4NYsYYpp");
-		session.addMessagePostedListener(new SlackMessagePostedListener() {
-			public void onEvent(SlackMessagePosted slackMessage, SlackSession session) {
-				if ( !slackMessage.getSender().isBot() ) {
-					String msg = slackMessage.getMessageContent();
-					String[] args = msg.split("[\\s]+");
-					if ( args.length == 2 && args[0].equals("!jenkins")) {
-						try {
-							String res = conn.get(args[1]+"?pretty=true");
-							session.sendMessage(slackMessage.getChannel(),res, null);
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					} else {
-						session.sendMessage(slackMessage.getChannel(),"Invalid Command!", null);
+	private String url = "https://fa15-cs427-082.cs.illinois.edu:8083/";
+	private JenkinsServer jenkins;
+	private JenkinsHttpClient conn;
+	private SlackSession session;
+	
+	
+    // Slack Message Posted Handler
+	private SlackMessagePostedListener postedHangdler = new SlackMessagePostedListener() {
+		public void onEvent(SlackMessagePosted slackMessage, SlackSession session) {
+			System.out.println();
+			if ( !slackMessage.getSender().isBot() ) {
+				String msg = slackMessage.getMessageContent();
+				String[] args = msg.split("[\\s]+");
+				if ( args.length == 2 && args[0].equals("!jenkins")) {
+					try {
+						String res = conn.get(args[1]+"?pretty=true");
+						session.sendMessage(slackMessage.getChannel(),res, null);
+					} catch (IOException e) {
+						session.sendMessage(slackMessage.getChannel(),"Cannot connect to the ", null);
+						e.printStackTrace();
 					}
+				} else {
+					session.sendMessage(slackMessage.getChannel(),"Invalid Command!", null);
 				}
 			}
-		});
+		}
+	};
+	
+	public BotDemo( String slackToken, String jenkinsURL ){
+		this(slackToken, jenkinsURL, null, null);
+	}
+	public BotDemo( String slackToken, String jenkinsURL, String userName, String password ) {
 		try {
-			session.connect();
+			if ( userName == null ) {
+				this.conn = new JenkinsHttpClient(new URI(jenkinsURL) );
+			} else {
+				this.conn = new JenkinsHttpClient(new URI(jenkinsURL), userName, password );
+			}
+			this.jenkins = new JenkinsServer(conn);
+		} catch (URISyntaxException e) {
+			System.out.println("Cannot connect to "+jenkinsURL);
+			e.printStackTrace();
+			return;
+		}
+		try {
+			ConnectToSlack(slackToken);
 		} catch (IOException e) {
+			System.out.println("Cannot connect to Slack!");
 			e.printStackTrace();
 		}
-
+	}
+	private void ConnectToSlack ( String token ) throws IOException {
+		this.session = SlackSessionFactory.createWebSocketSlackSession(token);
+		this.session.addMessagePostedListener(postedHangdler);
+		this.session.connect();
+	}
+	
+	public static void main(String[] args) {
+		new BotDemo("xoxb-13203396052-6U3vsnm89DThgtCw4NYsYYpp",
+				"https://fa15-cs427-032.cs.illinois.edu:8083/","zsong12", "w2jkscs427");
 		while (true) {
 			try {
 				Thread.sleep(1000);
